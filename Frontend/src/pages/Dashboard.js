455308b9fd2db6cade5fed98739d3be484346231
daniel-idowu-01 <danielidowu414@gmail.com>
2024-02-25 20:16:03 +0100

    updated pie chart

--- Frontend/src/pages/Dashboard.js
+++ Frontend/src/pages/Dashboard.js
@@ -11,26 +11,26 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 export const data = {
-  labels: ["Apple", "Knorr", "Shoop", "Green", "Purple", "Orange"],
+  labels: ["Shoe", "Phone", "Tie", "T-shirt", "Crocs", "Durag"],
   datasets: [
     {
       label: "# of Votes",
       data: [0, 1, 5, 8, 9, 15],
       backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+        "#339933",
+        "#F7CA51",
+        "#F7CA51",
+        "#007ACC",
+        "#9D38BD",
+        "#D7D7D7",
       ],
       borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
+        "#339933",
+        "#F7CA51",
+        "#F7CA51",
+        "#007ACC",
+        "#9D38BD",
+        "#D7D7D7",
       ],
       borderWidth: 1,
     },
