719dec645f735c6eafcf88dae6efdad02e52d72d
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-09 23:36:56 +0100

    feat: refactor signUp and login functions to include error handling and async/await syntax

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -5,45 +5,51 @@ const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-const signUp = async (req, res) => {
-  const { email, password, firstName, lastName, phoneNumber, imageUrl } =
-    req.body;
+const signUp = async (req, res, next) => {
+  try {
+    const { email, password, firstName, lastName, phoneNumber, imageUrl } =
+      req.body;
 
-  const isEmail = emailRegex.test(email);
-  const validPassword = passwordRegex.test(password);
+    const isEmail = emailRegex.test(email);
+    const validPassword = passwordRegex.test(password);
 
-  if (!isEmail) {
-    return res.status(400).json({ message: "Enter a valid email!" });
-  }
+    if (!isEmail) {
+      return res.status(400).json({ message: "Enter a valid email!" });
+    }
 
-  if (!validPassword) {
-    return res.status(400).json({
-      message:
-        "Password must have atleast one uppercase, one lowercase, one number, one symbol and must be more than 8 characters!",
+    if (!validPassword) {
+      return res.status(400).json({
+        message:
+          "Password must have atleast one uppercase, one lowercase, one number, one symbol and must be more than 8 characters!",
+      });
+    }
+
+    const user = await User.findOne({
+      email: req.body.email,
     });
+    if (user) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      phoneNumber,
+      imageUrl,
+    });
+
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
-
-  const user = await User.findOne({
-    email: req.body.email,
-  });
-  if (user) {
-    return res.status(400).json({ message: "User already exists!" });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  let registerUser = await User.create({
-    firstName,
-    lastName,
-    email,
-    password: hashedPassword,
-    phoneNumber,
-    imageUrl,
-  });
-  res.json(registerUser);
 };
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   let user, token, passwordMatch;
   try {
     const { email, password } = req.body;
@@ -74,6 +80,7 @@ const login = async (req, res) => {
     res.status(200).json({ success: true, data: token });
   } catch (error) {
     console.log(error);
+    next(error);
   }
 };
 
