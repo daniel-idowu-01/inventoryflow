aa2acd21f8ca3c0a4c3259471d244a6d29362b6a
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-10 22:53:27 +0100

    feat: enhance user model schema with type definitions and required fields

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    firstName: 'String',
-    lastName : 'String',
-    email: 'String',
-    password: 'String',
-    phoneNumber: 'Number',
-    imageUrl: 'String',
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  imageUrl: {
+    type: String,
+  },
 });
 
 const User = mongoose.model("users", UserSchema);
