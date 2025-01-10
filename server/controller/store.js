bfa6ae9e628265b574a3593d2e4a3de7103863de
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-10 23:27:03 +0100

    feat: enhance addStore and getAllStores functions with error handling and improved response messages

--- server/controller/store.js
+++ server/controller/store.js
@@ -1,33 +1,50 @@
 const Store = require("../models/store");
 
 // Add Store
-const addStore = async (req, res) => {
-  console.log(req.body);
-  const addStore = await new Store({
-    userId: req.body.userId,
-    name: req.body.name,
-    category: req.body.category,
-    address: req.body.address,
-    city: req.body.city,
-    image: req.body.image,
-  });
+const addStore = async (req, res, next) => {
+  try {
+    const { userId, name, category, address, city, image } = req.body;
 
-  addStore
-    .save()
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res.status(402).send(err);
+    if (!userId || !name || !category || !address || !city || !image) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const addStore = await Store.create({
+      userId,
+      name,
+      category,
+      address,
+      city,
+      image,
     });
+
+    if (!addStore) {
+      return res.status(400).json({ message: "Error adding store" });
+    }
+
+    res.status(201).json({ message: "Store added successfully!" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 // Get All Stores
-const getAllStores = async (req, res) => {
-  const findAllStores = await Store.find({ userId: req.params.userId }).sort({
-    _id: -1,
-  }); // -1 for descending;
-  res.json(findAllStores);
+const getAllStores = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+
+    const findAllStores = await Store.find({ userId }).sort({ _id: -1 }); // -1 for descending;\
+
+    if (!findAllStores) {
+      return res.status(400).json({ message: "No stores found" });
+    }
+
+    return res.status(200).json({ message: findAllStores });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 module.exports = { addStore, getAllStores };
