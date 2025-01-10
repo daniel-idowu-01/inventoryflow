cb7e621476842b166aefb9d1b61c95f68be147f3
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-10 22:56:38 +0100

    feat: move purchaseStock function to helpers and remove old file

--- server/controller/purchase.js
+++ server/controller/purchase.js
@@ -1,5 +1,5 @@
 const Purchase = require("../models/purchase");
-const purchaseStock = require("./purchaseStock");
+const purchaseStock = require('../utils/helpers')
 
 // Add Purchase Details
 const addPurchase = async (req, res, next) => {
